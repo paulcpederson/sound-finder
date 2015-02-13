e9249fc4de6898dd76cc5a329b74c7a0c3d399e3
Paul C Pederson <ppederson@esri.com>
2015-02-13 12:37:34 -0800

    add .each() method to $

--- src/js/$.js
+++ src/js/$.js
@@ -2,39 +2,31 @@
 * Create a new array like object of dom elements
 */
 function Query() {
+  this.each = (fn) => {
+    this.forEach(fn)
+    return this
+  }
   /**
   * Add a class to selected elements
   * @param {String} className The class name to add
   */
-  this.addClass = className => {
-    this.forEach(e => e.classList.add(className))
-    return this
-  }
+  this.addClass = className => this.each(e => e.classList.add(className))
   /**
   * Remove a class from selected elements
   * @param {String} className The class name to remove
   */
-  this.removeClass = className => {
-    this.forEach(e => e.classList.remove(className))
-    return this
-  }
+  this.removeClass = className => this.each(e => e.classList.remove(className))
   /**
   * Toggle a class from selected elements
   * @param {String} className The class name to toggle
   */
-  this.toggleClass = className => {
-    this.forEach(e => e.classList.toggle(className))
-    return this
-  }
+  this.toggleClass = className => this.each(e => e.classList.toggle(className))
   /**
   * Attach an event listener with a callback to the selected elements
   * @param {String} event Name of event, eg. "click", "mouseover", etc...
   * @param {Function} callback The function to call when the event is triggered
   */
-  this.on = (event, fn) => {
-    this.forEach(e => e.addEventListener(event, fn, false))
-    return this
-  }
+  this.on = (event, fn) => this.each(e => e.addEventListener(event, fn, false))
 }
 
 Query.prototype = Array.prototype
