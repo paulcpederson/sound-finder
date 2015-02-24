799ea0b1b685b4ec9b8fe9918551b64ba4e6eb98
Paul C Pederson <ppederson@esri.com>
2015-02-23 22:56:10 -0800

    more complete users template

--- src/js/index.js
+++ src/js/index.js
@@ -7,23 +7,24 @@ import $ from './$'
 
 let users = observable()
 let $users = document.querySelector('#users')
-var dd = new diffDOM();
+let dd = new diffDOM()
 
 users(u => {
   let tmp = $users.cloneNode(false)
   tmp.innerHTML = templates.users.render({users: u})
   dd.apply($users, dd.diff($users, tmp))
-  console.log($users)
 })
 
 $('.username-form').on('submit', (e) => {
   e.preventDefault()
   scroll.to(document.querySelector('.loader'))
+  $('.loader').addClass('show')
   let username = document.getElementById('username').value
   similarUsers(username).then(response => {
     users(response)
+    $('#users').addClass('show')
   }).catch(err => console.log(err))
 })
 
 // toggle info screen
-$('.info-toggle').on('click', () => $('.info').toggleClass('is-active'));
+$('.info-toggle').on('click', () => $('.info').toggleClass('is-active'))
