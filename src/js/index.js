e233cef2071ef2492b8edd3c9d38960de49734ff
Paul C Pederson <ppederson@esri.com>
2015-02-23 22:24:29 -0800

    one way data binding with patching

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,29 @@
 import similarUsers from './similar-users'
 import * as templates from './templates'
+import observable from 'observable'
+import diffDOM from 'diff-dom'
 import scroll from './scroll'
-import bind from './bind'
 import $ from './$'
 
-let users = [{ username: 'bob' }, { username: 'jill' }, { username: 'mary' }, { username: 'james' }];
-let model = bind({users: users}, $('#users')[0], templates.users)
+let users = observable()
+let $users = document.querySelector('#users')
+var dd = new diffDOM();
 
-//console.log(model)
-//model.users[0].username = 'phil'
+users(u => {
+  let tmp = $users.cloneNode(false)
+  tmp.innerHTML = templates.users.render({users: u})
+  dd.apply($users, dd.diff($users, tmp))
+  console.log($users)
+})
 
 $('.username-form').on('submit', (e) => {
   e.preventDefault()
-  scroll.to($('.loader'))
+  scroll.to(document.querySelector('.loader'))
   let username = document.getElementById('username').value
-  similarUsers(username).then( response => {
-    users = response
+  similarUsers(username).then(response => {
+    users(response)
   }).catch(err => console.log(err))
 })
 
 // toggle info screen
 $('.info-toggle').on('click', () => $('.info').toggleClass('is-active'));
-
