0100e45323bd2c70cb99b7aa4b5393387ec75e75
Paul C Pederson <ppederson@esri.com>
2015-03-06 21:58:51 -0800

    fix multiple assignment error

--- source/js/index.js
+++ source/js/index.js
@@ -7,8 +7,8 @@ import modelUsers from './models/similar-users.js'
 * Import Views
 */
 import viewLoader from './views/loader.js'
+import viewSearch from './views/search.js'
 import viewUsers from './views/similar-users.js'
-import viewInfo from './views/search.js'
 import viewInfo from './views/info.js'
 
 /**
