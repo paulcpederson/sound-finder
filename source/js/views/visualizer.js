e849e6870437728bbac96514c8f4dcf752df23fb
Paul C Pederson <paul.c.pederson@gmail.com>
2015-04-28 23:06:05 -0700

    begin visualizer

--- source/js/views/visualizer.js
+++ source/js/views/visualizer.js
@@ -1,9 +1,39 @@
 import events from 'pub-sub'
+import $ from '$'
 
-function draw(analyser, data) {
-  requestAnimationFrame(() => draw(analyser, data))
-  analyser.getByteFrequencyData(data)
-  //console.log(data)
+let canvas = document.getElementById('visualizer')
+let context = canvas.getContext('2d')
+let playing = false
+let barWidth = canvas.width / 16
+
+function resize() {
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+  barWidth = canvas.width / 16
 }
 
-events.on('visualize', draw)
+resize()
+
+// update the visualizer
+function draw(analyser, data) {
+  analyser.getByteFrequencyData(data)
+  context.clearRect(0, 0, canvas.width, canvas.height)
+  Object.keys(data).forEach(i => {
+    context.fillRect(i * barWidth, 0, barWidth, data[i])
+  })
+  if (playing) {
+    requestAnimationFrame(() => draw(analyser, data))
+  }
+}
+
+// three colors for waves in the pallette
+// #22A7C1
+// #1DA2BC
+// #3EC3DD
+
+$('window').on('resize', resize)
+
+events.on('visualize', (analyser, data) => {
+  playing = true
+  draw(analyser, data)
+})
