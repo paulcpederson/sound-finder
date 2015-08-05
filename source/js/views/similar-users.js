cb2c5f57fb26ae342fc60223833ea4019b2ee98b
Paul C Pederson <paul.c.pederson@gmail.com>
2015-08-04 23:17:02 -0700

    pretty stupid to use diffdom for something that isn 't updated

--- source/js/views/similar-users.js
+++ source/js/views/similar-users.js
@@ -1,13 +1,9 @@
 import view from '../../templates/users.dot'
-import DiffDOM from 'diff-dom'
 import events from 'pub-sub'
 import $ from '$'
 
 let users = $('#users')[0]
-let dd = new DiffDOM()
 
 events.on('users:updated', (data) => {
-  let tmp = users.cloneNode(false)
-  tmp.innerHTML = view({users: data})
-  dd.apply(users, dd.diff(users, tmp))
+  users.innerHTML = view({users: data})
 })
