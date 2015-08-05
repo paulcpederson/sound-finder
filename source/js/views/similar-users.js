8e0196db59cb123140f881fba8c5d8ed6e8fd314
Paul C Pederson <paul.c.pederson@gmail.com>
2015-08-05 09:51:18 -0700

    clean up avatar urls and resize them in view

--- source/js/views/similar-users.js
+++ source/js/views/similar-users.js
@@ -2,8 +2,15 @@ import view from '../../templates/users.dot'
 import events from 'pub-sub'
 import $ from '$'
 
-let users = $('#users')[0]
+let $users = $('#users')[0]
 
 events.on('users:updated', (data) => {
-  users.innerHTML = view({users: data})
+  let users = data.map(user => {
+    user.avatar_url = user.avatar_url.replace('-large', '-t500x500')
+    if (user.avatar_url.includes('default_avatar_large.png')) {
+      user.avatar_url = `https://sigil.cupcake.io/${user.username}?w=600`
+    }
+    return user
+  })
+  $users.innerHTML = view({users})
 })
