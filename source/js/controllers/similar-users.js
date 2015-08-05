2a23dbe7a847171beb92af29683523bb8540000c
Paul C Pederson <paul.c.pederson@gmail.com>
2015-08-05 10:47:20 -0700

    less hacky delegation check for similar users controller

--- source/js/controllers/similar-users.js
+++ source/js/controllers/similar-users.js
@@ -1,9 +1,10 @@
 import events from 'pub-sub'
 import $ from '$'
+import matches from 'dom-matches'
 
 // Users get added dynamically, so attach events to the users div
 $('#users').on('click', (e) => {
-  if (e.target.className === 'js-play-likes') {
+  if (matches(e.target, '.js-play-likes')) {
     e.preventDefault()
     let id = e.target.getAttribute('data-id')
     events.emit('player:new', id)
