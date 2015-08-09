c5f2ac0439c7ba04ebb381311efb77853ca25c7e
Paul C Pederson <paul.c.pederson@gmail.com>
2015-08-08 21:22:08 -0700

    get the basic loader actually on the page, not hooked up

--- source/js/views/loader.js
+++ source/js/views/loader.js
@@ -1,70 +1,34 @@
 import events from 'pub-sub'
 import $ from '$'
 
-let $loader = $('.loader')
 let $text = $('.loading-message')
 
 events.on('loader:update', ({percentage = 0, message = '', type = 'info'} = {}) => {
-  if (type === 'error') {
-    $loader.addClass('error')
-  } else {
-    $loader.addClass(`p${percentage}`)
-  }
   $text[0].textContent = message
 })
 
-var canvas
-var ctx
-var vertexes = []
-var diffPt = []
-var autoDiff = 1000
-var verNum = 250
-var canvasW = window.innerWidth
-var color1= "#6ca0f6"
-var color2 = "#367aec"
+var canvas = document.querySelector('.js-loader-canvas')
+var ctx = canvas.getContext('2d')
+var width = canvas.offsetWidth + 40
+var height = canvas.offsetHeight
 
-window.addEventListener("load", init )
+var size = 250
+var nodes = []
+var deltas = []
+var defaultDelta = 1000
 
-function resize() {
-  canvasW = document.getElementById('container').offsetWidth + 40
-  initCanvas(canvasW, window.innerHeight)
-  var cW = canvas.width
-  var cH = canvas.height
-  for (var i = 0; i < verNum; i++) {
-    vertexes[i] = new Vertex(cW / (verNum -1) * i, cH / 2, cH/2)
-  }
-  initDiffPt()
-  var win_3 = window.innerWidth / 3
-}
+resize()
 
-function init() {
-  resize()
-  var FPS = 30
-  var interval = 1000 / FPS >> 0
-  var timer = setInterval(update, interval);
-  window.addEventListener('resize', resize);
-  canvas.onmousedown = function (e) {
-    var mouseX,mouseY;
-    if (e) {
-      mouseX = e.pageX;
-      mouseY = e.pageY;
-    } else {
-      mouseX = event.x + document.body.scrollLeft;
-      mouseY = event.y + document.body.scrollTop;
-    }
-    if (window.innerHeight/2 - mouseY < 50 && window.innerHeight/2 - mouseY> -50) {
-      autoDiff = 1000;
-      if (mouseX<canvas.width-2) {
-        xx = 1 + Math.floor((verNum - 2) * mouseX / canvas.width);
-        diffPt[xx] = autoDiff;
-      }
-    }
-  }
-}
+var timer = setInterval(update, 30);
+window.addEventListener('resize', resize);
 
-function initDiffPt () {
-  for (var i = 0; i < verNum; i++) {
-    diffPt[i] = 0
+function resize () {
+  width = canvas.width = canvas.offsetWidth + 40;
+  height = canvas.height = canvas.offsetHeight
+  var length = width / (size - 1)
+  for (var i = 0; i < size; i++) {
+    nodes[i] = new Vertex(length * i, height / 2, height / 2)
+    deltas[i] = 0
   }
 }
 
@@ -73,35 +37,35 @@ var dd = 15;
 
 function update(){
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  autoDiff -= autoDiff*0.9;
-  diffPt[xx] = autoDiff;
+  defaultDelta -= defaultDelta * 0.9;
+  deltas[xx] = defaultDelta;
   for (var i=xx-1;i>0;i--) {
     var d = xx-i;
     if(d > dd)d=dd;
-    diffPt[i] -= (diffPt[i]-diffPt[i+1])*(1-0.01*d);
+    deltas[i] -= (deltas[i]-deltas[i+1])*(1-0.01*d);
   }
-  for(var i=xx+1;i<verNum;i++)
+  for(var i=xx+1;i<size;i++)
   {
     var d = i-xx;
     if(d > dd)d=dd;
-    diffPt[i] -= (diffPt[i]-diffPt[i-1])*(1-0.01*d);
+    deltas[i] -= (deltas[i]-deltas[i-1])*(1-0.01*d);
   }
 
-  for(var i = 0;i < vertexes.length;i++){
-    vertexes[i].updateY(diffPt[i]);
+  for(var i = 0; i < nodes.length; i++){
+    nodes[i].updateY(deltas[i]);
   }
 
   draw();
 
 }
 
-function draw(){
+function draw () {
   ctx.beginPath();
   ctx.moveTo(0,window.innerHeight);
-  ctx.fillStyle=color1;
-  ctx.lineTo(vertexes[0].x,vertexes[0].y);
-  for(var i = 1;i < vertexes.length;i++){
-    ctx.lineTo(vertexes[i].x,vertexes[i].y);
+  ctx.fillStyle = '#6ca0f6' // blue 1
+  ctx.lineTo(nodes[0].x,nodes[0].y);
+  for(var i = 1;i < nodes.length;i++){
+    ctx.lineTo(nodes[i].x,nodes[i].y);
   }
   ctx.lineTo(canvas.width,window.innerHeight);
   ctx.lineTo(0,window.innerHeight);
@@ -109,22 +73,15 @@ function draw(){
 
   ctx.beginPath();
   ctx.moveTo(0,window.innerHeight);
-  ctx.fillStyle=color2;
-  ctx.lineTo(vertexes[0].x+15,vertexes[0].y+5);
-  for(var i = 1;i < vertexes.length;i++){
-    ctx.lineTo(vertexes[i].x+15,vertexes[i].y+5);
+  ctx.fillStyle= '#367aec';
+  ctx.lineTo(nodes[0].x+15,nodes[0].y+5);
+  for(var i = 1;i < nodes.length;i++){
+    ctx.lineTo(nodes[i].x+15,nodes[i].y+5);
   }
   ctx.lineTo(canvas.width,window.innerHeight);
   ctx.lineTo(0,window.innerHeight);
   ctx.fill();
 
-}
-
-function initCanvas (width,height) {
-  canvas = document.getElementById("canvas");
-  canvas.width = width;
-  canvas.height = height;
-  ctx = canvas.getContext("2d");
 }
 
 function Vertex (x,y,baseY) {
@@ -142,4 +99,23 @@ Vertex.prototype.updateY = function (diffVal) {
   this.vy += this.targetY - this.y
   this.y += this.vy * this.friction;
   this.vy *= this.deceleration;
+}
+
+// this creates a "drop" on the surface
+canvas.onmousedown = function (e) {
+  var mouseX,mouseY;
+  if (e) {
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+  } else {
+    mouseX = event.x + document.body.scrollLeft;
+    mouseY = event.y + document.body.scrollTop;
+  }
+  if (window.innerHeight/2 - mouseY < 50 && window.innerHeight/2 - mouseY> -50) {
+    defaultDelta = 1000;
+    if (mouseX<canvas.width-2) {
+      xx = 1 + Math.floor((size - 2) * mouseX / canvas.width);
+      deltas[xx] = defaultDelta;
+    }
+  }
 }
