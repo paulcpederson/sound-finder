bc005c26fdf0b5a43cf389949b4dfb39d3fc98ec
Paul C Pederson <paul.c.pederson@gmail.com>
2015-08-12 21:55:59 -0700

    complete wave lib

--- source/js/lib/wave.js
+++ source/js/lib/wave.js
@@ -1,8 +1,14 @@
+/**
+* Create a wave on a 2d canvas
+*/
 import assign from 'object-assign'
 
-var defaultLevel = 50
-var defaultColors = ['rgba(0, 139, 188, .8)', 'rgba(33, 47, 75, .8)']
+const defaultLevel = 50
+const defaultColors = ['rgba(0, 139, 188, .8)', 'rgba(33, 47, 75, .8)']
 
+/**
+* Create an individual node on the wave
+*/
 function Vertex (x, y, baseY) {
   return {
     baseY,
@@ -21,26 +27,56 @@ function Vertex (x, y, baseY) {
   }
 }
 
+/**
+* Translate a percent to a pixel value on canvas coordinates
+*/
+function getHeight (percent, height) {
+  return height - Math.floor(height / 100 * percent)
+}
+
+/**
+* Wave factory function - accepts single options object
+* @param {Integer} level  Starting water level of wave (0 - 100)
+* @param {Array}   colors Array of two color stings
+* @param {Object}  canvas Canvas element to draw the wave on
+* @return Object
+*/
 function getWave ({level = defaultLevel, colors = defaultColors, canvas}) {
-  var w = {
+  let height = canvas.offsetHeight
+  let width = canvas.offsetWidth + 40
+
+  let w = {
     size: 300,
     active: 150,
     amplitude: 1000,
     nodes: [],
     deltas: [],
     ctx: canvas.getContext('2d'),
-    width: canvas.offsetWidth + 40,
-    height: canvas.offsetHeight,
-    addDrop (percent) {
-      w.amplitude = 1000
-      w.active = 170
+    level: getHeight(level, height),
+    /**
+    * Create a ripple effect on surface
+    * @param {Integer} x         position of drop (0 - 100)
+    * @param {Integer} amplitude Severity of surface distortion (pixels)
+    */
+    addDrop (x = 50, amplitude = 1250) {
+      w.amplitude = amplitude
+      w.active = Math.floor(w.size / 100 * x)
     },
+    /**
+    * Raise or lower water level
+    * @param {Integer} percent New water level (0 - 100)
+    */
     setLevel (percent) {
+      let baseY = getHeight(percent, height)
+      w.level = baseY
       w.nodes = w.nodes.map(n => {
-        n.baseY = n.baseY - 80
+        n.baseY = baseY
         return n
       })
     },
+    /**
+    * Draw the wave - call every frame
+    */
     draw () {
       let period = 18
       let deltas = w.deltas
@@ -62,11 +98,11 @@ function getWave ({level = defaultLevel, colors = defaultColors, canvas}) {
       w._drawPath(colors[1], 15, 5)
     },
     _resize () {
-      w.width = canvas.width = canvas.offsetWidth + 40;
-      w.height = canvas.height = canvas.offsetHeight
-      var length = w.width / (w.size - 1)
+      width = canvas.width = canvas.offsetWidth + 40;
+      height = canvas.height = canvas.offsetHeight
+      var length = width / (w.size - 1)
       for (var i = 0; i < w.size; i++) {
-        w.nodes[i] = Vertex(length * i, w.height/2, w.height/2)
+        w.nodes[i] = Vertex(length * i, w.level, w.level)
         w.deltas[i] = 0
       }
     },
@@ -74,10 +110,10 @@ function getWave ({level = defaultLevel, colors = defaultColors, canvas}) {
       let ctx = w.ctx
       let nodes = w.nodes
       ctx.beginPath()
-      ctx.moveTo(0, w.height)
+      ctx.moveTo(0, height)
       ctx.fillStyle = color
       ctx.lineTo(nodes[0].x + offsetX, nodes[0].y + offsetY)
-      nodes.forEach(n =>  {
+      nodes.forEach(n => {
         ctx.lineTo(n.x + offsetX, n.y + offsetY)
       })
       ctx.lineTo(canvas.width, canvas.height)
