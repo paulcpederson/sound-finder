eba836d06c0f28e2834191f7b250ac5b6d920204
Paul C Pederson <paul.c.pederson@gmail.com>
2015-08-14 17:11:43 -0700

    ensure that setInterval is turned off when users are loaded

--- source/js/views/loader.js
+++ source/js/views/loader.js
@@ -5,9 +5,11 @@ import Wave from '../lib/wave'
 let $text = $('.loading-message')
 var canvas = document.querySelector('.js-loader-canvas')
 var ctx = canvas.getContext('2d')
+let timer = false
 
 var wave = Wave({
   canvas,
+  viscosity: .95,
   level: 0
 })
 
@@ -16,11 +18,17 @@ function renderLoop () {
   wave.draw()
 }
 
-var timer = setInterval(renderLoop, 30)
+events.on('users:find', _ => {
+  let randomX = Math.floor((Math.random() * 100) + 1)
+  wave.addDrop(randomX, 2000)
+  timer = setInterval(renderLoop, 30)
+})
 
 events.on('loader:update', ({percentage = 0, message = '', type = 'info'} = {}) => {
-  $text[0].textContent = message
-  let randomX = Math.floor((Math.random() * 100) + 1)
-  wave.addDrop(randomX, 200)
+  $text[0].innerHTML = message
   wave.setLevel(percentage)
+})
+
+events.on('users:updated', _ => {
+  clearInterval(timer)
 })
