b88503b3ca507b92dccfffa8606218394f83e59b
Paul C Pederson <paul.c.pederson@gmail.com>
2015-08-18 23:07:30 -0700

    remove everything from the visualizer view

--- source/js/views/visualizer.js
+++ source/js/views/visualizer.js
@@ -1,40 +1,4 @@
 import events from 'pub-sub'
 import $ from '$'
 
-// let canvas = document.getElementById('visualizer')
-// let context = canvas.getContext('2d')
-// let playing = false
-// let barWidth = canvas.width / 16
-
-// function resize() {
-//   canvas.width = window.innerWidth
-//   canvas.height = window.innerHeight
-//   barWidth = canvas.width / 16
-// }
-
-// // update the visualizer
-// function draw(analyser, data) {
-//   analyser.getByteFrequencyData(data)
-//   context.clearRect(0, 0, canvas.width, canvas.height)
-//   Object.keys(data).forEach(i => {
-//     context.fillRect(i * barWidth, 0, barWidth, data[i])
-//   })
-//   if (playing) {
-//     requestAnimationFrame(() => draw(analyser, data))
-//   }
-// }
-
-// // three colors for waves in the pallette
-// // #22A7C1
-// // #1DA2BC
-// // #3EC3DD
-
-// $('window').on('resize', resize)
-
-events.on('play', (analyser, data) => {
-  playing = true
-  draw(analyser, data)
-  resize()
-})
-
-events.on('pause', () => playing = false)
+// analyser.getByteFrequencyData(data)
