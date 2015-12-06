2256c0d44653d2f1f842896374aee14e467ee6a7
Paul C Pederson <paul.c.pederson@gmail.com>
2015-12-06 10:41:42 -0800

    add hover over usernames to shapes

--- source/js/controllers/search.js
+++ source/js/controllers/search.js
@@ -1,14 +1,25 @@
 import events from 'pub-sub'
 import $ from '$'
 
+let searchTerm = ''
+let $input = document.querySelector('.js-search-input')
+
 // trigger search on submit
 $('.username-form').on('submit', (e) => {
   e.preventDefault()
-  events.emit('users:find', document.getElementById('username').value)
+  events.emit('users:find', $input.value)
   events.emit('pane', 2)
 })
 
 // back to search
 $('.back').on('click', () => {
   events.emit('pane', 1)
+})
+
+$('.js-shape').on('mouseover', (e) => {
+  $input.value = e.target.dataset.username
+})
+
+$('.js-shape').on('mouseout', (e) => {
+  $input.value = searchTerm
 })
