ec000c1ec9e66acb59a8226c9fb31b0dd9bfb563
Paul C Pederson <paul.c.pederson@gmail.com>
2015-12-19 13:56:23 -0800

    flip play and pause to make it more intuitive

--- source/js/controllers/player.js
+++ source/js/controllers/player.js
@@ -1,7 +1,7 @@
 import events from 'pub-sub'
 import $ from '$'
 
-$('.js-play').on('click', events.emit.bind(events, 'player:pause'))
-$('.js-pause').on('click', events.emit.bind(events, 'player:play'))
+$('.js-play').on('click', events.emit.bind(events, 'player:play'))
+$('.js-pause').on('click', events.emit.bind(events, 'player:pause'))
 $('.js-next').on('click', events.emit.bind(events, 'player:next'))
 $('.js-prev').on('click', events.emit.bind(events, 'player:prev'))
