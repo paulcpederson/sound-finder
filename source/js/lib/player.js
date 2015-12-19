a831a99b1190479a1843f5fd39677509c24030a1
Paul C Pederson <paul.c.pederson@gmail.com>
2015-12-19 13:59:26 -0800

    player lib now exports the same player instance, rather than exposing a factory function

--- source/js/lib/player.js
+++ source/js/lib/player.js
@@ -1,32 +1,29 @@
 import assign from 'object-assign'
 
-function getPlayer (element) {
-  let context = new (window.AudioContext || window.webkitAudioContext)
-  let source = context.createMediaElementSource(element)
-  let analyser = context.createAnalyser()
-  analyser.fftSize = 32
-  let dataStream = new Uint8Array(analyser.frequencyBinCount)
-  source.connect(analyser)
-  analyser.connect(context.destination)
+let element = document.querySelector('.js-player')
+let context = new (window.AudioContext || window.webkitAudioContext)
+let source = context.createMediaElementSource(element)
+let analyser = context.createAnalyser()
+analyser.fftSize = 32
+let dataStream = new Uint8Array(analyser.frequencyBinCount)
+source.connect(analyser)
+analyser.connect(context.destination)
 
-  var player = {
-    analyser,
-    dataStream,
-    current: 0,
-    next () {
-      this.current++
-      this.loadTrack()
-    },
-    prev () {
-      this.current--
-      this.loadTrack()
-    },
-    loadTrack () {
-      this.setAttribute('src', `${this.tracks[this.current].stream_url}?client_id=739b39925c3cc275aeb03837ff27762c`)
-    }
+var player = {
+  analyser,
+  dataStream,
+  current: 0,
+  next () {
+    this.current++
+    this.loadTrack()
+  },
+  prev () {
+    this.current--
+    this.loadTrack()
+  },
+  loadTrack () {
+    this.setAttribute('src', `${this.tracks[this.current].stream_url}?client_id=739b39925c3cc275aeb03837ff27762c`)
   }
-
-  return assign(element, player)
 }
 
-export default getPlayer
+export default assign(element, player)
