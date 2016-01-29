f0a1d21d7724365adf32ac59e058459967b63574
Paul C Pederson <paul.c.pederson@gmail.com>
2016-01-29 08:25:48 -0800

    working route parsing

--- source/js/routes.js
+++ source/js/routes.js
@@ -1,4 +1,21 @@
 import routeMatcher from 'route-matcher'
+import events from 'pub-sub'
 
-let url = document.location.pathname
-console.log(url)
+
+let match = routeMatcher.routeMatcher
+let url = document.location.pathname + '/'
+url = url.replace('//', '/')
+
+let home = match('/').parse(url)
+let user = match('/:username/').parse(url)
+let play = match('/:username/play/').parse(url)
+
+if (home) {
+  console.log('show home page')
+} else if (user) {
+  console.log('matches user: ', user)
+} else if (play) {
+  console.log('play user: ', play)
+} else {
+  console.log('show 404')
+}
