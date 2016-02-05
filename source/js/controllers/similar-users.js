5582fcc0d6a161379c1e4209a7cf4aed00296caf
Paul C Pederson <paul.c.pederson@gmail.com>
2016-02-04 21:37:40 -0800

    fix problem with clicking on similar users not actually firing up player

--- source/js/controllers/similar-users.js
+++ source/js/controllers/similar-users.js
@@ -1,16 +1,13 @@
 import events from 'pub-sub'
 import pushState from '../lib/push-state'
 import $ from '$'
-import matches from 'dom-matches'
+import closest from 'dom-closest'
 
 // Users get added dynamically, so attach events to the users div
 $('.js-user-wrap').on('click', (e) => {
-  if (matches(e.target, '.js-play-likes')) {
-    e.preventDefault()
-    let id = e.target.dataset.id
-    let username = e.target.dataset.username
-    events.emit('player:new', id)
-    events.emit('pane', 4)
-    pushState(`${username}'s favorites`, `/${username}/play/`)
-  }
+  e.preventDefault()
+  let user = closest(e.target, '.js-play-likes').dataset
+  events.emit('player:new', user.id)
+  events.emit('pane', 4)
+  pushState(`${user.username}'s favorites`, `/${user.username}/play/`)
 })
