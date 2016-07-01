165137b1755ea71329d5db3eadbdef6bd411d0f7
Paul C Pederson <paul.c.pederson@gmail.com>
2016-07-01 10:23:17 -0700

    make top nav mostly work

--- source/js/controllers/similar-users.js
+++ source/js/controllers/similar-users.js
@@ -5,10 +5,11 @@ import closest from 'dom-closest'
 
 // Users get added dynamically, so attach events to the users div
 $('.js-user-wrap').on('click', (e) => {
+
   e.preventDefault()
   let user = closest(e.target, '.js-play-likes')
   let username = user.getAttribute('data-username')
   events.emit('player:new', user.getAttribute('data-id'))
-  events.emit('pane', 4)
   pushState(`${username}'s favorites`, `/${username}/play/`)
+  events.emit('pane', 4)
 })
--- source/js/views/nav.js
+++ source/js/views/nav.js
@@ -1,28 +1,40 @@
+import routeMatcher from 'route-matcher'
 import events from 'pub-sub'
 import $ from '$'
 
 let navs = $('.js-nav')
 let site = $('.js-nav-site')
 let similar = $('.js-nav-similar')
-let similarName = $('.js-nav-similar-name')
+let similarName = document.querySelector('.js-nav-similar-name')
 let user = $('.js-nav-user')
-let userName = $('.js-nav-user-name')
+let userName = document.querySelector('.js-nav-user-name')
+let userWrap = document.querySelector('.js-user-wrap')
 
 /**
  * Update the top nav when changing panes
  * @param {Int} n Pane to navigate to
  */
 events.on('pane', (n) => {
-  navs.addClass('hidden')
+  navs.addClass('hide')
+  let name = userWrap.getAttribute('data-username')
+
   if (n > 1) {
-    site.removeClass('hidden')
+    site.removeClass('hide')
   }
-  if (n > 2) {
-    similar.removeClass('hidden')
-    // set text of similarName to current similar search
+
+  if (name && n > 2) {
+    similar.removeClass('hide')
+    similarName.innerHTML = `similar to ${name}`
+    similarName.setAttribute('href', `/${name}/`)
   }
+
   if (n > 3) {
-    user.removeClass('hidden')
-    // set text of user to currently playing
+    // parse user name from url
+    let url = document.location.pathname + '/'
+    url = url.replace('//', '/')
+    let play = routeMatcher.routeMatcher('/:username/play/').parse(url)
+    userName.innerHTML = play.username
+    userName.setAttribute('href', `https://soundcloud.com/${play.username}/`)
+    user.removeClass('hide')
   }
 })
